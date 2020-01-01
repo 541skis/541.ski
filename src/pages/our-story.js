8b10c4dc0af77c679531fc84163c20c6d590dd95
Cade Scroggins <git@cade.me>
2020-01-01 14:19:47 -0800

    fix typo

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -10,15 +10,15 @@ const OurStoryPage = () => (
     <Header hero="Our Story" />
     <Content>
       <Text as="p" variant="paragraph">
-        541 was started out necessity and a love of skiing. Owners, Brad Duffy
-        and Jesse Scroggins, were born into ski racing roots which attributes to
-        their desire to charge the mountain full tilt. In search for a ski to
-        live up to their expectations as an &ldquo;all mountain&rdquo;
-        one-quiver, they were consistently left disappointed. So, Brad, designer
-        and production engineer, and Jesse, craftsman and amateur designer,
-        teamed up to create a ski to fill the void. The Adobo was
-        born&mdash;541&rsquo;s flagship ski. Every design is built, tested and
-        proven on the year-round slopes of Mt. Hood, OR by the owners. If we
+        541 was started out of necessity and a love of skiing. Owners, Brad
+        Duffy and Jesse Scroggins, were born into ski racing roots which
+        attributes to their desire to charge the mountain full tilt. In search
+        for a ski to live up to their expectations as an &ldquo;all
+        mountain&rdquo; one-quiver, they were consistently left disappointed.
+        So, Brad, designer and production engineer, and Jesse, craftsman and
+        amateur designer, teamed up to create a ski to fill the void. The Adobo
+        was born&mdash;541&rsquo;s flagship ski. Every design is built, tested
+        and proven on the year-round slopes of Mt. Hood, OR by the owners. If we
         don&rsquo;t like it, we don&rsquo;t sell it! Try a pair and push as hard
         as you can. 541 skis will not let you&nbsp;down.
       </Text>
